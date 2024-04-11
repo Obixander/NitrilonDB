a4f4b0a9a3e162d6232060d83ceb56ea392c075c
obixander <noahhauge@gmail.com>
2024-04-11 13:08:56 +0200

    made API work

---
+++ NitrilonDB/Controllers/EventController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nitrilon.Entities;
+
+namespace NitrilonDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {            
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put(Event eventToUpdate)
+        {
+            
+            return Ok(eventToUpdate);
+        }
+
+        [HttpGet]
+        public IEnumerable<Event> GetAll()
+        {
+            List<Event> events = new()
+            {
+                new() {Id=1}, new() {Id=2}
+            };
+            return events;
+        }
+
+        [HttpGet("{id}")]        
+        public ActionResult<Event> Get(int id)
+        {
+            Event e = null;
+            if (id == 3)
+            {
+                e = new Event { Id = 3 };
+            }
+            else
+            {
+                return NotFound($"The requested event tih id {id} was not found");
+            }
+            return e;
+        }
+        [HttpPost]
+        public IActionResult Add(Event newEvent)
+        {
+            try
+            {
+                //do that db stuff
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+        } 
+
+
+    }
+}
