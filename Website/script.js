25986ae53017203ed5bd0bcf4eecabae4d85f627
obixander <noahhauge@gmail.com>
2024-04-15 14:47:33 +0200

    Updated Frontend and API Settigns

--- Website/script.js
+++ Website/script.js
@@ -2,7 +2,7 @@ let btn1 = document.querySelector('#btn1');
 let btn2 = document.querySelector('#btn2');
 let btn3 = document.querySelector('#btn3');
 let Div = document.querySelector('#Events');
-
+let Cooldown = 0;
 
 
 // console.log("button clicked")
@@ -31,31 +31,31 @@ let Div = document.querySelector('#Events');
 //     });
 
 //     console.log(test);
-    
-    
+
+
 // });
 
 
 //Methods for Posting to Database on button Click
 //add A method for this and use the button id for the method
 const EventRatingURL = 'https://localhost:7239/api/EventRating';
-btn1.addEventListener('click', function (event) {
-   event.preventDefault();
+btn1.addEventListener('click', function (OnClick) {
+   OnClick.preventDefault();
    let id = 1;
    AddRating(id);
 });
-btn2.addEventListener('click', function (event) {
-   event.preventDefault();
+btn2.addEventListener('click', function (OnClick) {
+   OnClick.preventDefault();
    let id = 2;
    AddRating(id);
 });
-btn3.addEventListener('click', function (event) {
-   event.preventDefault();
+btn3.addEventListener('click', function (OnClick) {
+   OnClick.preventDefault();
    let id = 3;
    AddRating(id);
 });
 
-function AddRating(id) {   
+function AddRating(id) {
    const requestOptions = {
       method: 'post',
       headers: {
@@ -65,21 +65,60 @@ function AddRating(id) {
          //Change this EventId later
          "EventId": "2",
 
-         "RatingID": id,        
+         "RatingID": id,
       })
    };
-   
+
    fetch(EventRatingURL, requestOptions)
       .then(response => {
          if (!response.ok) {
-         throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok');
          }
          return response.text();
       })
       .then(data => {
-        
+
       })
       .catch(error => {
          console.error('Error:', error);
       });
+
+
+   AddOverlay();
+
+
+
+}
+
+function AddOverlay() {
+   const Timer = Date.now();
+   
+   if (Timer > Cooldown)
+  {   
+   let Overlay = document.createElement('div');
+   Overlay.classList.toggle('overlay');
+
+   let text = document.createElement("h1")
+   text.classList.toggle('text');
+   text.textContent = "Tak for dit Feedback!";
+   Overlay.append(text);
+
+   let Body = document.querySelector("body")
+   Body.append(Overlay);
+
+   
+
+   setTimeout(function () {
+      Overlay.remove()
+      text.remove()
+   }, 5000);
+   Cooldown = Date.now() ;
+   Cooldown = Math.floor(Cooldown  + 5000);
+   console.log(Cooldown)
+   console.log(Math.floor(Timer - Cooldown))
+   }
+   else {
+
+   }
+
 }
