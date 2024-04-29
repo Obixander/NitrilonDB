342b89f83ab945baa56165230a717e4566812d8d
obixander <noahhauge@gmail.com>
2024-04-29 13:48:10 +0200

    Added API controller and class member

---
+++ Nitrilon.Entities/Member.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitrilon.Entities
+{
+    public class Member
+    {
+        private string name;
+        private string phoneNumber;
+        private string email;
+        private DateTime date;
+        private int MembershipId;
+
+        public Member(string name, string phoneNumber, string email, DateTime date, int membershipId1)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Date = date;
+            MembershipId1 = membershipId1;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Email { get => email; set => email = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public int MembershipId1 { get => MembershipId; set => MembershipId = value; }
+    }
+}
---
+++ NitrilonDB/Controllers/MemberController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NitrilonDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+
+
+    }
+}
