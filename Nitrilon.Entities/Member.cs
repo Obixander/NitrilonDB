f05cd3d569e28645b6e08d2b417ec6b852a999d2
obixander <noahhauge@gmail.com>
2024-04-29 14:01:37 +0200

    added class membership

--- Nitrilon.Entities/Member.cs
+++ Nitrilon.Entities/Member.cs
@@ -8,25 +8,28 @@ namespace Nitrilon.Entities
 {
     public class Member
     {
+        private int memberId;
         private string name;
         private string phoneNumber;
         private string email;
         private DateTime date;
-        private int MembershipId;
+        private int membershipId;
 
-        public Member(string name, string phoneNumber, string email, DateTime date, int membershipId1)
+        public Member(int memberId, string name, string phoneNumber, string email, DateTime date, int membershipId)
         {
+            MemberId = memberId;
             Name = name;
             PhoneNumber = phoneNumber;
             Email = email;
             Date = date;
-            MembershipId1 = membershipId1;
+            MembershipId = membershipId;
         }
 
         public string Name { get => name; set => name = value; }
         public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
         public string Email { get => email; set => email = value; }
         public DateTime Date { get => date; set => date = value; }
-        public int MembershipId1 { get => MembershipId; set => MembershipId = value; }
+        public int MembershipId { get => membershipId; set => membershipId = value; }
+        public int MemberId { get => memberId; set => memberId = value; }
     }
 }
---
+++ Nitrilon.Entities/Membership.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitrilon.Entities
+{
+    public class Membership
+    {
+        private int membershipId;
+        private string name;
+        private string description;
+
+        public Membership(int membershipId, string name, string description)
+        {
+            MembershipId = membershipId;
+            Name = name;
+            Description = description;
+        }
+
+        public int MembershipId { get => membershipId; set => membershipId = value; }
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+    }
+}
